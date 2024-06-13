5503f9d5e04db038715693bf2cf18ea7801cf3fc
David Garcia <david.garcia@wachter.com>
2024-06-12 20:38:28 -0600

    format documents

--- src/app/4-data-binding/data-binding/data-binding.component.ts
+++ src/app/4-data-binding/data-binding/data-binding.component.ts
@@ -1,27 +1,25 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms'; 
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrl: './data-binding.component.scss'
+  styleUrl: './data-binding.component.scss',
 })
-
 export class DataBindingComponent {
-  oneWayText: string = "";
-  twoWaysText: string = "";
+  oneWayText: string = '';
+  twoWaysText: string = '';
 
-clickMe() {
-  alert("I Was Triggered by an Event");
-}
-
-ngAfterContentInit(): void {
-  this.oneWayText = this.twoWaysText = "Hello World";
-}
-
-twoWaysChangeText($event: Event) {
-  const htmlElement = $event.target as HTMLInputElement;
-  this.twoWaysText = htmlElement.value || "";
+  clickMe() {
+    alert('I Was Triggered by an Event');
   }
 
+  ngAfterContentInit(): void {
+    this.oneWayText = this.twoWaysText = 'Hello World';
+  }
+
+  twoWaysChangeText($event: Event) {
+    const htmlElement = $event.target as HTMLInputElement;
+    this.twoWaysText = htmlElement.value || '';
+  }
 }
